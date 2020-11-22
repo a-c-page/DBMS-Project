e070c463ba5d084545f22f3619b31b43c362cfa1
Taha Hashmat <taha.hashmat@ontariotechu.net>
2020-11-23 00:11:57 +0300

    loginpage initial

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ selectElement('.close').addEventListener('click', () => {
         selectElement('.nav-list').classList.remove('active');
         });
  
+
+//LogIn Register Form Swap Function
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
