57927f5f29605fbd1c52e3c9d60c4677ff92131b
Austin <austincolepage@hotmail.com>
2020-11-23 13:09:41 -0500

    for u taha <3

--- home.php
+++ home.php
@@ -1,103 +1,76 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Van Gogh Executive Travel </title>
-    <!--Ion Icons-->
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-   <!--Google Fonts-->
-   <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
-   <link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
-    <!-- linkig the css file "main.css" -->
-    <link rel="stylesheet" href="style.css">
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Van Gogh Executive Travel </title>
+<!--Ion Icons-->
+<link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+	<!--Google Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
+			<!-- linkig the css file "main.css" -->
+			<link rel="stylesheet" href="style.css">
+			</head>
+			<body>
+				<!-- Header which contains nav bar and image-->
+				<header>
+					<div class="container">
+						<nav>
+              <img src="images/profile.png" class="profile">
+							<div class="nav-brand"></div>
+							<div class="menu-icons open">
+								<i class="icon ion-md-menu"></i>
+							</div>
 
-</head>
-<body>
-<!-- Header which contains nav bar and image-->
-    <header>
+							<ul class="nav-list">
 
-        <div class="container">
-            <nav>
-                <div class="nav-brand">
+								<div class="menu-icons close">
+									<i class="icon ion-md-close"></i>
+								</div>
 
-                </div>
+								<li class="nav-item">
+									<a href="#" class="nav-link">Home</a>
+									<br></br>
+									<br></br>
+									<br></br>
+								</li>
 
-                <div class="menu-icons open">
-                    <i class="icon ion-md-menu"></i>
-                </div>
+								<li class="nav-item">
+									<a href="#" class="nav-link">Our Showroom</a>
+									<br></br>
+									<br></br>
+									<br></br>
+								</li>
 
-                <ul class="nav-list">
-                    <div class="menu-icons close">
-                        <i class="icon ion-md-close"></i>
-                    </div>
+								<li class="nav-item">
+									<a href="#" class="nav-link">Bookings</a>
+									<br></br>
+									<br></br>
+									<br></br>
+								</li>
 
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Home</a>
-                    <br>
-                    </br>
-                    <br>
-                </br>
-                <br>
-            </br>
+								<li class="nav-item">
+									<a href="#" class="nav-link">Contact</a>
+									<br></br>
+									<br></br>
+									<br></br>
+								</li>
 
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Our Showroom</a>
-                     <br>
-                    </br>
-                    <br>
-                </br>
-                <br>
-            </br>
+								<li class="nav-item">
+									<a href="login.php" class="nav-link">Login/Register</a>
+									<br></br>
+									<br></br>
+									<br></br>
+								</li>
 
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Bookings</a>
-                    <br>
-                    </br>
-                    <br>
-                </br>
-                <br>
-            </br>
+							</ul>
 
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Contact</a>
-                    <br>
-                    </br>
-                    <br>
-                </br>
-                <br>
-            </br>
-
-                    </li>
-                    <li class="nav-item">
-                        <a href="login.php" class="nav-link">LOG-IN & Register</a>
-
-                    <br>
-                    </br>
-                    <br>
-                </br>
-                <br>
-            </br>
-
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
-    </header>
-
-    <main>
-
-        <section class="firstsection">
-            <div class="container">
-                <div class="companyname">
-                    <h3> Van Gogh Executive Travel</h3>
-                    <p>
+						</nav>
+					</div>
+				</header>
+				<main>
+					<section class="firstsection">
+						<div class="container">
+							<div class="companyname">
+								<h3> Van Gogh Executive Travel</h3>
+								<p>
                         Guaranteed Luxury and Comfort
                     </p>
                 </div>
@@ -128,14 +101,13 @@
                             wmd2[kdcjoccmalsmwecipoe
                             jwdpwcm;mckq;pdkqd[odk
                         </p>
-                        <br>
-                        <br/>
-                    </div>
-
-                    <!--second-->
-                    <div class="activities-grid-item secondheading">
-                        <h1>Canada's Most Luxurious & Exclusive Fleet</h1>
-                        <p>
+									<br>
+										<br/>
+									</div>
+									<!--second-->
+									<div class="activities-grid-item secondheading">
+										<h1>Canada's Most Luxurious & Exclusive Fleet</h1>
+										<p>
                             jmwejcwpcjwpcojwpcjwpcjk
                             wpjvwjcwvpwcwcnmwpcmwpcj
                             nw;pcnwcw pcmwc2pcmwcp
@@ -143,14 +115,13 @@
                             wmd2[kdcjoccmalsmwecipoe
                             jwdpwcm;mckq;pdkqd[odk
                         </p>
-                        <br>
-                        <br/>
-                    </div>
-
-                    <!--third-->
-                    <div class="activities-grid-item thirdheading">
-                        <h1>Seamless, State of the Art Service</h1>
-                        <p>
+										<br>
+											<br/>
+										</div>
+										<!--third-->
+										<div class="activities-grid-item thirdheading">
+											<h1>Seamless, State of the Art Service</h1>
+											<p>
                             jmwejcwpcjwpcojwpcjwpcjk
                             wpjvwjcwvpwcwcnmwpcmwpcj
                             nw;pcnwcw pcmwc2pcmwcp
@@ -158,68 +129,47 @@
                             wmd2[kdcjoccmalsmwecipoe
                             jwdpwcm;mckq;pdkqd[odk
                         </p>
-                        <br>
-                        <br/>
-                    </div>
-
-                </div>
-
-            </div>
-        </section>
-
-        <section class="companyservices">
-            <div class="container">
-
-                <div class="mainheading">
-                    <br></br>
-                    <h3>Unique Services Offered by Us</h3>
-                    <br></br>
-                    <h1> Striving to set the benchmark in everything
+											<br>
+												<br/>
+											</div>
+										</div>
+									</div>
+								</section>
+								<section class="companyservices">
+									<div class="container">
+										<div class="mainheading">
+											<br></br>
+											<h3>Unique Services Offered by Us</h3>
+											<br></br>
+											<h1> Striving to set the benchmark in everything
                         our Company Sets Out to Achieve is a Testament
                         to Our Examplary Services </h1>
-                </div>
-
-                <div class="activities-grid">
-
-                    <!--first-->
-                    <div class="activities-grid-item fourthheading">
-                        <h1>Apply online, get approved in a few hours with
+										</div>
+										<div class="activities-grid">
+											<!--first-->
+											<div class="activities-grid-item fourthheading">
+												<h1>Apply online, get approved in a few hours with
                             Your vehicle ready for you on arrival.</h1>
-
-                        <br>
-                        <br/>
-                    </div>
-
-                    <!--second-->
-                    <div class="activities-grid-item fifthheading">
-                        <h1> Insight with Interactive Spec Sheets</h1>
-
-                        <br>
-                        <br/>
-                    </div>
-
-                    <!--third-->
-                    <div class="activities-grid-item sixthheading">
-                        <h1>Welcome Addition of Electric Vehicles</h1>
-                        <h1></h1>
-
-                        <br>
-                        <br/>
-                    </div>
-
-                </div>
-
-            </div>
-        </section>
-
-    </main>
-
-
-
-
-
-
-
-    <script type="text/javascript" src="script.js"></script>
-</body>
-</html>
+												<br>
+													<br/>
+												</div>
+												<!--second-->
+												<div class="activities-grid-item fifthheading">
+													<h1> Insight with Interactive Spec Sheets</h1>
+													<br>
+														<br/>
+													</div>
+													<!--third-->
+													<div class="activities-grid-item sixthheading">
+														<h1>Welcome Addition of Electric Vehicles</h1>
+														<h1></h1>
+														<br>
+															<br/>
+														</div>
+													</div>
+												</div>
+											</section>
+										</main>
+										<script type="text/javascript" src="script.js"></script>
+									</body>
+								</html>
