079468f6544f331495c61b52c4f681e8ff916d63
Taha Hashmat <taha.hashmat@ontariotechu.net>
2020-11-24 07:27:39 +0300

    updates database

--- database/dbms-project.sql
+++ database/dbms-project.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2020 at 01:57 AM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: localhost
+-- Generation Time: Nov 24, 2020 at 05:27 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,16 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `bookings`
 --
 
-DROP TABLE IF EXISTS `bookings`;
-CREATE TABLE IF NOT EXISTS `bookings` (
+CREATE TABLE `bookings` (
   `bookingID` int(11) NOT NULL,
   `customerID` int(11) NOT NULL,
   `VIN` varchar(45) NOT NULL,
   `startDate` varchar(45) NOT NULL,
   `endDate` varchar(45) NOT NULL,
   `Price` double NOT NULL,
-  `paymentType` varchar(45) NOT NULL,
-  PRIMARY KEY (`bookingID`,`customerID`,`VIN`)
+  `paymentType` varchar(45) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='	';
 
 --
@@ -62,8 +59,7 @@ INSERT INTO `bookings` (`bookingID`, `customerID`, `VIN`, `startDate`, `endDate`
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE `customer` (
   `customerID` varchar(45) NOT NULL,
   `licenseNumber` varchar(45) NOT NULL,
   `firstName` varchar(45) NOT NULL,
@@ -72,8 +68,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `Street` varchar(45) NOT NULL,
   `City` varchar(45) NOT NULL,
   `Postal` varchar(45) NOT NULL,
-  `Unit` int(11) NOT NULL,
-  PRIMARY KEY (`customerID`,`licenseNumber`)
+  `Unit` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -98,8 +93,7 @@ INSERT INTO `customer` (`customerID`, `licenseNumber`, `firstName`, `lastName`, 
 -- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE IF NOT EXISTS `employee` (
+CREATE TABLE `employee` (
   `employeeID` int(11) NOT NULL,
   `SSN` int(11) NOT NULL,
   `firstName` varchar(45) NOT NULL,
@@ -108,8 +102,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `Street` varchar(45) NOT NULL,
   `Unit` int(11) NOT NULL,
   `Postal` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
-  PRIMARY KEY (`employeeID`,`SSN`)
+  `Email` varchar(45) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -134,16 +127,14 @@ INSERT INTO `employee` (`employeeID`, `SSN`, `firstName`, `lastName`, `City`, `S
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -159,8 +150,7 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `passwo
 -- Table structure for table `vehicle`
 --
 
-DROP TABLE IF EXISTS `vehicle`;
-CREATE TABLE IF NOT EXISTS `vehicle` (
+CREATE TABLE `vehicle` (
   `VIN` varchar(45) NOT NULL,
   `plateNumber` varchar(45) NOT NULL,
   `Colour` varchar(45) NOT NULL,
@@ -170,8 +160,7 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
   `Mileage` int(11) NOT NULL,
   `Status` varchar(45) NOT NULL,
   `Photo` varchar(255) NOT NULL,
-  `Price` decimal(65,0) NOT NULL,
-  PRIMARY KEY (`VIN`,`plateNumber`)
+  `Price` decimal(65,0) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -179,17 +168,61 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
 --
 
 INSERT INTO `vehicle` (`VIN`, `plateNumber`, `Colour`, `Year`, `Make`, `Model`, `Mileage`, `Status`, `Photo`, `Price`) VALUES
-('JTKDE3B73A0347847', 'ABCD001', 'Yellow', 2018, 'Lamborghini', 'Aventador', 777, 'Available', 'yaventador18.jpg', '0'),
-('3FAHP0JA9CR100470', 'BCDE002', 'Blue', 2020, 'Bugatti', 'Chiron', 201, 'Unavailable', 'bchiron20.jpg', '0'),
-('1GNFK16K5RJ334512', 'CDEF003', 'White', 2019, 'Tesla', 'Model X', 1900, 'Available', 'wmodelx19.jpg', '0'),
-('WBAVC53598FZ90864', 'DEFG004', 'Black', 2019, 'Lamborghini', 'Huracan Performante', 506, 'Available', 'bhuracan19.jpg', '0'),
-('WAUKH68D11A004771', 'EFGH005', 'Purple', 2010, 'Lamborghini', 'Murcielago', 2034, 'Unavailable', 'pmurcielago10.jpg', '0'),
-('1FTFW1CF7DKD95982', 'FGHI006', 'Red', 2020, 'Ferrari', 'F8', 102, 'Unavailable', 'rf820.jpg', '0'),
-('G3URU3ISIFHRI679232', 'HWWO467', 'Charcoal', 2019, 'Aston Martin', 'DBS', 1500, 'Available', 'charcoaldb519.jpg', '0'),
-('HJ3JRFJE922JS892109', 'KAIC234', 'Black', 2020, 'Bentley', 'Bentyga', 450, 'Available', 'bbentayga20.jpg', '0'),
-('IEE433JFJDJEJ458922', 'QIWL920', 'Yellow', 2019, 'Lamborghini', 'Urus', 982, 'Available', 'yurus20.jpg', '0'),
-('HSUW20DHJW348910', 'HWQL201', 'Black', 2018, 'Rolls Royce', 'Wraith', 3485, 'Unavailable', 'bwraith18.jpg', '0'),
+('JTKDE3B73A0347847', 'ABCD001', 'Yellow', 2018, 'Lamborghini', 'Aventador', 777, 'Available', 'yaventador18.jpg', '265'),
+('3FAHP0JA9CR100470', 'BCDE002', 'Blue', 2020, 'Bugatti', 'Chiron', 201, 'Unavailable', 'bchiron20.jpg', '400'),
+('1GNFK16K5RJ334512', 'CDEF003', 'White', 2019, 'Tesla', 'Model X', 1900, 'Available', 'wmodelx19.jpg', '100'),
+('WBAVC53598FZ90864', 'DEFG004', 'Black', 2019, 'Lamborghini', 'Huracan Performante', 506, 'Available', 'bhuracan19.jpg', '275'),
+('WAUKH68D11A004771', 'EFGH005', 'Purple', 2010, 'Lamborghini', 'Murcielago', 2034, 'Unavailable', 'pmurcielago10.jpg', '125'),
+('1FTFW1CF7DKD95982', 'FGHI006', 'Red', 2020, 'Ferrari', 'F8', 102, 'Unavailable', 'rf820.jpg', '200'),
+('G3URU3ISIFHRI679232', 'HWWO467', 'Charcoal', 2019, 'Aston Martin', 'DBS', 1500, 'Available', 'charcoaldb519.jpg', '180'),
+('HJ3JRFJE922JS892109', 'KAIC234', 'Black', 2020, 'Bentley', 'Bentyga', 450, 'Available', 'bbentayga20.jpg', '275'),
+('IEE433JFJDJEJ458922', 'QIWL920', 'Yellow', 2019, 'Lamborghini', 'Urus', 982, 'Available', 'yurus20.jpg', '250'),
+('HSUW20DHJW348910', 'HWQL201', 'Black', 2018, 'Rolls Royce', 'Wraith', 3485, 'Unavailable', 'bwraith18.jpg', '350'),
 ('GHIWDXGHWEDGHJEDGH', 'JKDJKEDHJKED', 'Black', 1990, 'Honda', 'Civic', 999999, 'Unavailable', 'honda.jpg', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`bookingID`,`customerID`,`VIN`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customerID`,`licenseNumber`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`employeeID`,`SSN`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`VIN`,`plateNumber`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
